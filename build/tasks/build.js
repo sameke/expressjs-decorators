4e731b322c38771a88ea3a356926784f2ea563f3
Ben Johnson <weagle08@gmail.com>
2019-04-03 21:45:16 -0500

    upgrade dependencies

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,31 +1,30 @@
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
 var changed = require('gulp-changed');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var paths = require('../paths');
-var assign = Object.assign || require('object.assign');
 var notify = require('gulp-notify');
 var typescript = require('gulp-typescript');
+require('./clean');
 
 // transpiles changed es6 files to SystemJS format
 // the plumber() call prevents 'pipe breaking' caused
 // by errors from other gulp plugins
 // https://www.npmjs.com/package/gulp-plumber
 var typescriptCompiler = typescriptCompiler || null;
-gulp.task('build-system', function() {
-  if(!typescriptCompiler) {
+gulp.task('build-system', function () {
+  if (!typescriptCompiler) {
     typescriptCompiler = typescript.createProject('tsconfig.json', {
       "typescript": require('typescript')
     });
   }
 
   return gulp.src(paths.source)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(changed(paths.output, {extension: '.ts'}))
-    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(changed(paths.output, { extension: '.ts' }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(typescriptCompiler())
-    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: './'}))
+    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: './' }))
     .pipe(gulp.dest(paths.output));
 });
 
@@ -38,10 +37,4 @@ gulp.task('move', () => {
 // in ./clean.js), then runs the build-system
 // and build-html tasks in parallel
 // https://www.npmjs.com/package/gulp-run-sequence
-gulp.task('build', function(callback) {
-  return runSequence(
-    'clean',
-    ['build-system', 'move'],
-    callback
-  );
-});
+gulp.task('build', gulp.series('clean', gulp.parallel('build-system', 'move')));
